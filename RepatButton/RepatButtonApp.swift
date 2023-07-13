1b617db4093c2078f0bb343b368f7c26f0f7cfd8
bilal simsek <xyzbilal@gmail.com>
2023-07-13 16:25:17 +0300

    Initial Commit

---
+++ RepatButton/RepatButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepatButtonApp.swift
+//  RepatButton
+//
+//  Created by Bilal SIMSEK on 13.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct RepatButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
